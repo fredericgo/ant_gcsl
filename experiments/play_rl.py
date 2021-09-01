d2e30aa2d49f7412776fa0763eb03afb88fdb569
Fred Go <fredericgo@gmail.com>
2021-09-01 12:17:45 +0800

    ok

---
+++ experiments/play_rl.py
@@ -0,0 +1,104 @@
+
+import gym
+import numpy as np
+import torch
+import pathlib
+import sys
+sys.path.append('./')
+
+import imageio
+from rl.networks import GaussianPolicy
+
+output_dir = '/tmp', 
+env_name = 'ant_fixed_goal'
+
+model_dir = 'runs/2021-08-31_17-39-13_SAC_ant_fixed_goal_Gaussian'
+video_file = 'video.mp4'
+hidden_size = 512
+gpu = True
+seed = 0
+
+# Envs
+from gcsl import envs
+# Algo
+from gcsl.algo import buffer, gcsl, variants, networks
+import pathlib
+
+if not gpu:
+    print('Not using GPU. Will be slow.')
+
+torch.manual_seed(seed)
+np.random.seed(seed)
+
+env = envs.create_env(env_name)
+
+action_dim = env.action_space.shape[0]
+policy = GaussianPolicy(env.obs_dims, action_dim, env.goal_dims, hidden_size, env.action_space)
+
+
+max_path_length = 50
+
+
+def load_policy(model_dir):
+    policy_path = pathlib.Path(model_dir) / "models" / "actor"
+    policy.load_state_dict(torch.load(policy_path, map_location=torch.device('cpu')))
+
+def select_action(policy, state, goal, evaluate=False):
+    state = torch.FloatTensor(state).unsqueeze(0)
+    goal = torch.FloatTensor(goal).unsqueeze(0)
+
+    if evaluate is False:
+        action, _, _ = policy.sample(state, goal)
+    else:
+        _, _, action = policy.sample(state, goal)
+    return action.detach().cpu().numpy()[0]
+
+def sample_trajectory(writer, greedy=False, noise=0):
+    goal_state = env.sample_goal()
+    goal = env.extract_goal(goal_state)
+    qpos = np.concatenate([[0, 0],goal[:(env.env.model.nq-2)]])
+    qvel = goal[(env.env.model.nq-2):]
+    env.set_state(qpos, qvel)
+
+    for _ in range(100):
+        env.render()
+
+    
+    states = []
+    actions = []
+
+    state = env.reset()
+    done = False
+    for t in range(max_path_length):
+        env.render()
+
+        states.append(state)
+        if done:
+            state = env.reset()
+
+        observation = env.observation(state)
+        horizon = np.arange(max_path_length) >= (max_path_length - 1 - t) # Temperature encoding of horizon horizon[None],
+        action = select_action(policy, observation, goal)
+        
+        action = np.clip(action, env.action_space.low, env.action_space.high)
+        
+        actions.append(action)
+        state, _, done, _ = env.step(action)
+       
+    return np.stack(states), np.array(actions), goal_state
+
+load_policy(model_dir)
+
+writer = imageio.get_writer(video_file, fps=30) 
+for _ in range(10):
+    sample_trajectory(writer)
+
+#sample_init(noise=1, render=True)
+
+if __name__ == "__main__":
+    params = {
+        'seed': [0],
+        'env_name': ['ant'], #['ant'],
+        'gpu': [True],
+    }
+    pass
--- gcsl/envs/mujoco/ant.py
+++ gcsl/envs/mujoco/ant.py
@@ -38,10 +38,10 @@ class Env(mujoco_env.MujocoEnv, utils.EzPickle):
         num_jnt = self.model.njnt
         noise = 1
 
-        qpos = self.init_qpos
+        qpos = self.init_qpos.copy()
         qpos[7:] = np.array([0.,  1,   0.,   -1.,   0.,   -1.,   0.,  1.])
         qpos += np.random.randn(nq) * noise
-        qvel = self.init_qvel + np.random.randn(self.model.nv) * noise
+        qvel = self.init_qvel.copy() + np.random.randn(self.model.nv) * noise
         self.set_state(qpos, qvel)
         return self._get_obs()
 
