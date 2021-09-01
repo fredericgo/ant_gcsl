1ef6732b098993691716cf122e45f885b4505694
frederic go <fredericstgo@gmail.com>
2021-09-02 00:20:02 +0800

    reacher goal

--- gcsl/envs/reacher_goal_env.py
+++ gcsl/envs/reacher_goal_env.py
@@ -27,10 +27,6 @@ class ReacherGoalEnv(GymGoalEnvWrapper):
         )
         self.goal = np.concatenate([qpos, qvel])
 
-    def _extract_z(self, goal):
-        return goal[..., :1]
-     
-
     def goal_distance(self, state, goal_state):
         if self.goal_metric == 'euclidean':
             qdiff = (self.extract_goal(state) -
@@ -39,7 +35,6 @@ class ReacherGoalEnv(GymGoalEnvWrapper):
             #return np.linalg.norm(qdiff, axis=-1) 
         else:
             raise ValueError('Unknown goal metric %s' % self.goal_metric)
-
 
     def get_diagnostics(self, trajectories, desired_goal_states):
         """
@@ -52,14 +47,14 @@ class ReacherGoalEnv(GymGoalEnvWrapper):
         Returns:
             An Ordered Dictionary containing k,v pairs to be logged
         """
-        z_distances = np.array(
-            [self.z_distance(trajectories[i], np.tile(desired_goal_states[i], (trajectories.shape[1], 1))) for i
+        distances = np.array(
+            [self.goal_distance(trajectories[i], np.tile(desired_goal_states[i], (trajectories.shape[1], 1))) for i
              in range(trajectories.shape[0])])
     
      
         return OrderedDict([
-            ('mean final z dist', np.mean(z_distances[:, -1])),
-            ('median final z dist', np.median(z_distances[:, -1])),
+            ('mean final angle dist', np.mean(distances[:, -1])),
+            ('median final angle dist', np.median(distances[:, -1])),
         ])
 
 
